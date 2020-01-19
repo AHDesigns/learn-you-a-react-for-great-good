6ed8c67422beb333fcb01f06a3e0d90848b15d1e
Lucy Skowron <lucy_skowron@hotmail.com>
2020-01-19 17:38:36 +0000

    add button component

--- src/02-Button/comp.story.js
+++ src/02-Button/comp.story.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import './style.css';
-import Component from './component';
+import Button from './component';
 
 export default {
   title: '02-Button',
-  component: Component,
+  component: Button,
 };
 
 export const Text = () => (
   <div className='text'>
-    <Component onClick={action('clicked')} />
+    <Button onClick={(e) => { console.log("Clickedy click") }}>I am a Button</ Button>
+    <Button
+      onClick={action('I was clicked! 🐼')}
+      type="primary"
+    >
+      I am a Button
+    </ Button>
+    <Button
+      type="disabled"
+      onClick={action('I was clicked! 🐼')}
+    >
+      I am a panda
+    </ Button>
   </div>
 );
--- src/02-Button/component.js
+++ src/02-Button/component.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './style.css';
 
-function Component() {
-  return <div className='test'>Component</div>;
+// text
+// type= primary | secondary | disabled
+// onClick = () =>  {}
+
+// button-type
+// button-primary
+
+function Button({ children, onClick, type = 'primary' }) {
+  if (!['primary', 'disabled'].includes(type)) {
+    console.warn('INVALID PROPD PASSED TO BUTTON')
+  }
+  return (
+    <p>
+      <button
+        onClick={onClick}
+        className={`button-${type}`}
+        disabled={type === 'disabled'}
+      >
+        {children}
+      </button>
+    </p>
+  );
 }
 
-export default Component;
+export default Button;
